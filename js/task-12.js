423459d0cfd52340b56b11e9e729b34de64df790
NataliaZgurska <143435353+NataliaZgurska@users.noreply.github.com>
2023-12-03 11:57:38 +0200

    add_task12

---
+++ js/task-12.js
@@ -0,0 +1,14 @@
+// /**
+//  *? Напишіть функцію updateObject, яка приймає об'єкт та повертає
+//  *? новий об'єкт без вказаного параметра
+//  *? Очікуваний результат updateObject({a: 1, b: 2, c: 3}, 'b') => {a: 1}
+//  */
+
+const obj = { a: 1, b: 2, c: 3 };
+function updateObject(newObj, key) {
+    const resObj = newObj
+    delete resObj[key];
+    return resObj
+}
+
+console.log(updateObject(obj, 'c'))
